4ce2cfbcedfdf46e67aab24189e57bf3e7e61429
Marcus Eriksson <eriksson.marcus1@outlook.com>
2020-08-04 13:41:31 +0200

    Application layer structure

---
+++ CA.Solution/CA.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using CA.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses(); 
+    }
+}
---
+++ CA.Solution/CA.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CA.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
