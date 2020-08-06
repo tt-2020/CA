d5d1e5a21f54ba969efad9d677732a4a21fbb822
Marcus Eriksson <eriksson.marcus1@outlook.com>
2020-08-06 12:25:22 +0200

    Presentation layer finish

---
+++ CA.Solution/CA.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CA.Application.Interfaces;
+using CA.Application.ViewModels;
+using CA.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CA.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
